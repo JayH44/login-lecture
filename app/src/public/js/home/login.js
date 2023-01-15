f5c9fcc2fa4a2e747fc0c17f2478e517a533f44b
JayH <slamhjh@gmail.com>
2023-01-15 23:04:20 +0900

    로그인 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,36 @@
 'use strict';
 
-console.log('hello');
+const id = document.querySelector('#id'),
+  password = document.querySelector('#password'),
+  loginBtn = document.querySelector('button');
+
+loginBtn.addEventListener('click', login);
+
+function login() {
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+
+  console.log(req);
+
+  fetch('/login', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        location.href = '/';
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error('로그인 중 에러 발생');
+      console.error(new Error('로그인 중 에러 발생'));
+    });
+}
