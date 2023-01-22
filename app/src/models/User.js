19acbde6004ff34bdf95a65425c7ef6c055eab00
JayH <slamhjh@gmail.com>
2023-01-22 21:31:09 +0900

    User 모델 구현

---
+++ app/src/models/User.js
@@ -0,0 +1,22 @@
+'use strict';
+const UserStorage = require('./UserStorage');
+
+class User {
+  constructor(body) {
+    this.body = body;
+  }
+
+  login() {
+    const { id, password } = UserStorage.getUserInfo(this.body.id);
+
+    if (id) {
+      if (id === this.body.id && password === this.body.password) {
+        return { success: true };
+      }
+      return { success: false, msg: '비밀번호가 같지 않습니다.' };
+    }
+    return { success: false, msg: '사용자 아이디가 없습니다.' };
+  }
+}
+
+module.exports = User;
